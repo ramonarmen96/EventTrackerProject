ee178a6fb58edffba9068b40bef9aae976aeb945
ramonarmen96 <ramonarmen96@gmail.com>
2021-04-02 15:06:03 -0600

    Spring boot and JPA projects linked

---
+++ HorrorHouse/src/main/java/com/skilldistillery/movie/HorrorHouseApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorrorHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorrorHouseApplication.class, args);
+	}
+
+}
---
+++ HorrorHouse/src/test/java/com/skilldistillery/movie/HorrorHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorrorHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
